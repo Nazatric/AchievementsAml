ca23d2c6008832923a1e22f367ff8aea078e5b57
Nazatric <nazakat1930@gmail.com>
2026-07-07 14:43:01 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,9 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := achievementsaml
+LOCAL_MODULE := achievementsaml
 LOCAL_SRC_FILES := main.cpp logger.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
+LOCAL_CFLAGS += -O2 -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += .
 LOCAL_LDLIBS += -llog
+
 include $(BUILD_SHARED_LIBRARY)
